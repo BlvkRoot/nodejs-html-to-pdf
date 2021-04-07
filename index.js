ecd06f04d6818b2c93151359cf7a901b688294bd
blvkroot <hsalukamba17@gmail.com>
2021-04-07 13:41:22 +0100

    Estilização do pdf apartir de um arquivo html.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const htmlToPDF = require('html-pdf-node');
+const fs = require('fs');
+const { resolve } = require('path');
 
 const app = express();
 const PORT = 8000 || process.env.PORT;
-const options = { path: 'test.pdf', format: 'A4' };
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -16,10 +17,30 @@ app.get("/", (req, res) => {
 
 app.post("/convert-pdf", (req, res) => {
   const { nome, bi, nif, fone, morada } = req.body;
+  const options = { path: `${nome}.pdf`, format: 'A4', margin: {left: '20px', right: '20px'}};
+  let returnedTestFile = '';
 
-  let file = { content: `<h1>${nome}</h1>`};
+  // let file = { content: `
+  //                         <div style="margin-left: 20px; margin-right: 20px; display: flex; justify-content: center; align-items: center; flex-direction: column;">
+  //                           <div style="padding-bottom: 20px;"><label style="display: flex; justify-content: space-between; width: 500px;"><span>Nome:</span> <input type="text" value="${nome}" style="border: none;" readonly/></label></div>
+  //                           <div style="padding-bottom: 20px;"><label style="display: flex; justify-content: space-between; width: 500px;"><span>BI:</span> <input type="text" value="${bi}" style="border: none;" readonly/></label></div>
+  //                           <div style="padding-bottom: 20px;"><label style="display: flex; justify-content: space-between; width: 500px;"><span>NIF:</span> <input type="text" value="${nif}" style="border: none;" readonly/></label></div>
+  //                           <div style="padding-bottom: 20px;"><label style="display: flex; justify-content: space-between; width: 500px;"><span>Telefone:</span> <input type="text" value="${fone}" style="border: none;" readonly/></label></div>
+  //                           <div style="padding-bottom: 20px;"><label style="display: flex; justify-content: space-between; width: 500px;"><span>Morada:</span> <input type="text" value="${morada}" style="border: none;" readonly/></label></div>
+  //                         </div>
+  //             `};
 
-  htmlToPDF.generatePdf(file, options).then(pdfBuffer => {
+  let testFile = fs.readFileSync(resolve(`${__dirname}/views/test.html`), {
+    encoding: 'utf-8'   
+  });
+
+  returnedTestFile = testFile.replace('a__', nome)
+  returnedTestFile = returnedTestFile.replace('b__', bi);
+  returnedTestFile = returnedTestFile.replace('c__', nif);
+  returnedTestFile = returnedTestFile.replace('d__', fone);
+  returnedTestFile = returnedTestFile.replace('e__', morada);
+
+  htmlToPDF.generatePdf({content: `${returnedTestFile}`}, options).then(pdfBuffer => {
       console.log("", pdfBuffer);
   });
 
